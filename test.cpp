acd72891bb502503153a8a76fe125e381f509549
Hiro Watanabe <a07054617811@icloud.com>
2025-08-08 18:20:49 +0900

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc,char *argv)
+{
+    std::cout << "undefine push" << std::endl
+    while (1){
+        std::cout << "action" std::endl;
+        i++;
+    }
+    return (??)
+};;;;
+;;;;;;;;;;;;;;;;;;
