8b83ec4cc45e33ea03c5c31ca51b8b769390f146
MoriP <kohei.moriyama426@gmail.com>
2025-08-08 20:56:13 +0900

    WIP: client

--- echoServer/include/Client.hpp
+++ echoServer/include/Client.hpp
@@ -2,6 +2,8 @@
 #define CLIENT_HPP
 
 #include <string>
+#include <vector>
+#include <set>
 
 class Client
 {
@@ -11,17 +13,38 @@ private:
 	std::string _username;
 	std::string _hostname;
 	std::string _realname;
-	// bool authenticated;
+	bool _authenticated;
+	bool _registered;
 	std::string _buf;
-
+	std::set<std::string> _channels; // joined channel
 
 public:
 	Client(int socket);
 	~Client();
 
+	// setter
+	void setNickname(const std::string& nickname);
+	void setUsername(const std::string& username);
+	void setHostname(const std::string& hostname);
+	void setRealname(const std::string& realname);
+	void setAuthenticated(const bool& athenticated);
+	void setRegistered(const bool& registered);
+
+	// getter
 	int getSocket(void) const;
+	const std::string& getNickname(void) const;
+	const std::string& getUserkname(void) const;
+	const std::string& getHostname(void) const;
+	const std::string& getRealname(void) const;
+
+	// 認証状態管理
+	bool isAuthenticated(void) const;
+	bool isRegistered(void) const;
+
+	// メッセージ処理
+	void appendToBuffer(const std::string& data);
+	std::vector<std::string> extractCommand(void);
+	void sendReply(const std::string& reply);
 };
-
-
 
 #endif
--- echoServer/srcs/Client.cpp
+++ echoServer/srcs/Client.cpp
@@ -9,7 +9,67 @@ Client::~Client()
 {
 }
 
+void Client::setNickname(const std::string& nickname)
+{
+	this->_nickname = nickname;
+}
+
+void Client::setUsername(const std::string& username)
+{
+	this->_username = username;
+}
+
+void Client::setHostname(const std::string& hostname)
+{
+	this->_hostname = hostname;
+}
+
+void Client::setRealname(const std::string& realname)
+{
+	this->_realname = realname;
+}
+
+void Client::setAuthenticated(const bool& athenticated)
+{
+	this->_authenticated = athenticated;
+}
+
+void Client::setRegistered(const bool& registered)
+{
+	this->_registered = registered;
+}
+
 int Client::getSocket(void) const
 {
 	return this->_socket;
+}
+
+const std::string& Client::getNickname(void) const
+{
+	return this->_nickname;
+}
+
+const std::string& Client::getUserkname(void) const
+{
+	return this->_username;
+}
+
+const std::string& Client::getHostname(void) const
+{
+	return this->_hostname;
+}
+
+const std::string& Client::getRealname(void) const
+{
+	return this->_realname;
+}
+
+bool Client::isAuthenticated(void) const
+{
+	return this->_authenticated;
+}
+
+bool Client::isRegistered(void) const
+{
+	return this->_registered;
 }
