be41d42f70268c9667180aefe56d85b54530ebfd
Hiro Watanabe <a07054617811@icloud.com>
2025-08-08 21:35:31 +0900

    #5 add test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostreeeeeeeeeeeeeeeem>
+
+int main(int argc,char *argv)
+{
+    private:
+    std::cout << "hello" std::endl
+    while (1)
+    {
+        std::cout << incliment << i << std::endl;
+        i++
+    }
+    return ?
+}
+????
