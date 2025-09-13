d8affc22a0e699c7429bf5f90659efe0bf69738c
Hiro Watanabe <a07054617811@icloud.com>
2025-09-13 16:18:35 +0900

    fix channnel

--- srcs/Server/Client/Command.cpp
+++ srcs/Server/Client/Command.cpp
@@ -6,23 +6,54 @@ void Server::serverInfo()
 	std::cout << *this << std::endl;
 }
 
-// === EXIT === 
+// === EXIT ===
 void Server::disconnectClient(int clientFd)
 {
+    std::map<int, Client *>::iterator it_client = _clients.find(clientFd);
+    if (it_client != _clients.end())
+    {
+        Client *client = it_client->second;
+
+        // クライアントが参加している全チャンネルから削除
+        std::set<std::string> clientChannels = client->getChannels();
+        for (std::set<std::string>::iterator ch_it = clientChannels.begin();
+             ch_it != clientChannels.end(); ++ch_it)
+        {
+            Channel *channel = getChannel(*ch_it);
+            if (channel)
+            {
+                channel->removeMember(client);
+
+                // チャンネルが空になったら削除
+                if (channel->isEmpty())
+                {
+                    std::map<std::string, Channel*>::iterator channel_it = _channels.find(*ch_it);
+                    if (channel_it != _channels.end())
+                    {
+                        delete channel_it->second;
+                        _channels.erase(channel_it);
+                    }
+                }
+            }
+        }
+
+        delete client;
+        _clients.erase(it_client);
+    }
+
+    // pollFdsから削除
     for (size_t i = 0; i < _pollFds.size(); ++i)
     {
         if (_pollFds[i].fd == clientFd)
         {
             _pollFds.erase(_pollFds.begin() + i);
-            std::map<int, Client *>::iterator it_client = _clients.find(clientFd);
-            if (it_client != _clients.end())
-            {
-                delete it_client->second;
-                _clients.erase(it_client);
-            }
             break;
         }
     }
+
+    // クライアントバッファも削除
+    _clientBuffers.erase(clientFd);
+
     close(clientFd);
 }
 
